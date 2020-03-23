d2f0351a3a3b1bf1db0f29796661ff1ad58d147f
thetti27 <57488820+thetti27@users.noreply.github.com>
2020-03-24 01:48:27 +0530

    Update bonus_calculator.c

--- bonus_calculator.c
+++ bonus_calculator.c
@@ -4,32 +4,28 @@
 int main(void)
 
 {
-    float bs,bonus; //declaring the basic salary
-    float a =1.05, b = 1.1, c = 1.15;
-    int ec; // declare employee category
+
+
+    double bs,bonus; //declaring the basic salary and bonus
+    float a = 1.05, b = 1.1, c = 1.15; // Declaring bonus multipliers
+    int ec; // declaring employee category
     printf("Please your employee category(1,2 or 3) and basic salary: \n");
-    scanf("%f %f", &ec, &bs);  // read the keyboard input for salary
+    scanf("%f %lf", &ec, &bs);  // read the keyboard input for salary
 
 
-    //Switch statement using employee category
-    switch (ec)
-    {
-    case 1:
-        bonus= float multipy(float bs, float a);
-        printf( "Your bonus is %f",&bonus);
-        break;
-    case 2:
-        bonus= float multipy(float bs, float b);
-        printf( "Your bonus is %f.",&bonus);
-        break;
-    case 3:
-        bonus= float multipy(float bs, float b);
-        printf( "Your bonus is %f.",&bonus);
-        break;
-    default:
-        printf( "Employee category invlaid. Please enter 1,2 or 3.");
-        break;
-    }
-  
+        // If-else statement to calculate and print bonus
+        if(ec == 1)
+        bonus= bs*a;
+        printf( "Your bonus is %lf",&bonus);
+            else if(ec == 2)
+            bonus= bs*b;
+            printf( "Your bonus is %lf.",&bonus);
+                else if (ec == 3)
+                bonus= bs*c;
+                printf( "Your bonus is %lf.",&bonus);
+                    else
+                    printf( "Employee category invlaid. Please enter 1,2 or 3.");
+                    
+
 return 0;
 }
